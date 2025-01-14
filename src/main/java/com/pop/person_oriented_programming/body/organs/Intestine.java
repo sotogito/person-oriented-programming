d4cf91505e49beab749e1741719df4248f1e7ddc
sotogito <108535526+sotogito@users.noreply.github.com>
2025-01-15 01:30:07 +0900

    feat : 장기(장) 추가 생성

---
+++ src/main/java/com/pop/person_oriented_programming/body/organs/Intestine.java
@@ -0,0 +1,11 @@
+package com.pop.person_oriented_programming.body.organs;
+
+import com.pop.person_oriented_programming.body.MajorOrgan;
+
+public class Intestine {
+
+    public MajorOrgan getOrganType() {
+        return MajorOrgan.INTESTINE;
+    }
+
+}
