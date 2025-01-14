c518ed734cb081f0becfcbcb5555ab7d7fc50a14
sotogito <108535526+sotogito@users.noreply.github.com>
2025-01-14 23:49:07 +0900

    Initial commit

---
+++ src/main/java/com/pop/person_oriented_programming/PersonOrientedProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.pop.person_oriented_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonOrientedProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonOrientedProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pop/person_oriented_programming/PersonOrientedProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pop.person_oriented_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonOrientedProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
