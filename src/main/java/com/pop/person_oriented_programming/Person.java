d697ffb75603dc760a24cbcb2b15a5ed9d3bd741
sotogito <108535526+sotogito@users.noreply.github.com>
2025-01-15 01:25:16 +0900

    feat : Person 작성

---
+++ src/main/java/com/pop/person_oriented_programming/Person.java
@@ -0,0 +1,19 @@
+package com.pop.person_oriented_programming;
+
+import com.pop.person_oriented_programming.body.Organ;
+import com.pop.person_oriented_programming.profile.Profile;
+import com.pop.person_oriented_programming.soul.Identity;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private final Identity identity;
+    private final Profile profile;
+    private final List<Organ> organs = new ArrayList<>();
+
+    public Person(Identity identity, Profile profile) {
+        this.identity = identity;
+        this.profile = profile;
+    }
+
+}
