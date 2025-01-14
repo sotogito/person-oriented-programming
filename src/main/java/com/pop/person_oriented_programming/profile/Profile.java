efa9c6add6ec59bd3b4e9e16734499e7670880de
sotogito <108535526+sotogito@users.noreply.github.com>
2025-01-15 01:29:38 +0900

    feat : 프로필 생성

---
+++ src/main/java/com/pop/person_oriented_programming/profile/Profile.java
@@ -0,0 +1,16 @@
+package com.pop.person_oriented_programming.profile;
+
+import java.time.LocalDateTime;
+
+public class Profile {
+    private int age;
+    private float height;
+    private float weight;
+
+    public Profile(int age, float height, float weight) {
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+}
